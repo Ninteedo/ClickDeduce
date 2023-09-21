0caa81ddc65375f3215a8be7f6e379a52fec2964
Richard Holmes <richard.gordon.holmes@gmail.com>
2023-09-21 15:52:34 +0100

    Add first version of ClickDeduceLanguage.scala

---
+++ src/main/scala/language/ClickDeduceLanguage.scala
@@ -0,0 +1,118 @@
+package language
+
+/**
+ * Parent trait for all languages designed to be loaded in ClickDeduce.
+ */
+trait ClickDeduceLanguage {
+  /**
+   * A variable name.
+   *
+   * Case sensitive.
+   */
+  type Variable = String
+
+  /**
+   * The evaluation environment at a particular point.
+   *
+   * Contains variables with bound values.
+   */
+  type Env = Map[Variable, Value]
+
+  /**
+   * The type environment at a particular point.
+   *
+   * Contains variables with bound types.
+   */
+  type TypeEnv = Map[Variable, Type]
+
+  /**
+   * An unevaluated expression.
+   */
+  abstract class Expr
+
+  /**
+   * A value resulting from an expression being evaluated.
+   */
+  abstract class Value
+
+  /**
+   * An error resulting from an expression being evaluated.
+   */
+  abstract class EvalError extends Value
+
+  /**
+   * The type of a value.
+   */
+  abstract class Type
+
+  /**
+   * An error resulting from an expression being type checked.
+   */
+  abstract class TypeError extends Type
+
+  /**
+   * Function which evaluates an `Expr` to a `Value`, given an environment.
+   *
+   * @param e The `Expr` to evaluate.
+   * @param env The environment to evaluate the `Expr` in.
+   * @return The `Value` resulting from evaluating the `Expr`.
+   */
+  def eval(e: Expr, env: Env): Value
+
+  /**
+   * Function which evaluates an `Expr` to a `Value`, given an empty environment.
+   *
+   * Equivalent to calling <code>eval(e, Map()).</code>
+   *
+   * @param e The `Expr` to evaluate.
+   * @return The `Value` resulting from evaluating the `Expr`.
+   */
+  def eval(e: Expr): Value = {
+    eval(e, Map())
+  }
+
+  /**
+   * Function to perform type checking on an `Expr` in the given type environment.
+   *
+   * @param e    The `Expr` on which type checking needs to be performed.
+   * @param tenv The type environment in which type checking is done.
+   * @return The `Type` of the expression after type checking.
+   */
+  def typeCheck(e: Expr, tenv: TypeEnv): Type
+
+  /**
+   * Overloaded type checking function that performs type checking on an `Expr` in an empty type environment.
+   *
+   * Equivalent to calling <code>typeCheck(e, Map()).</code>
+   *
+   * @param e The `Expr` on which type checking needs to be performed.
+   * @return The `Type` of the expression after type checking.
+   */
+  def typeCheck(e: Expr): Type = {
+    typeCheck(e, Map())
+  }
+
+  /**
+   * Function to create a human-readable string representation of an `Expr`.
+   *
+   * @param e The `Expr` to be pretty printed.
+   * @return A `String` representing the pretty printed expression.
+   */
+  def pretty_print(e: Expr): String
+
+  /**
+   * Function to create a human-readable string representation of a `Type`.
+   *
+   * @param t The `Type` to be pretty printed.
+   * @return A `String` representing the pretty printed type.
+   */
+  def pretty_print(t: Type): String
+
+  /**
+   * Function to create a human-readable string representation of a `Value`.
+   *
+   * @param v The `Value` to be pretty printed.
+   * @return A `String` representing the pretty printed value.
+   */
+  def pretty_print(v: Value): String
+}
