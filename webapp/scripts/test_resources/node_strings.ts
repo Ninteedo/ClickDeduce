cb0835b45db699ece2fe86132d6689068b0f456f
Richard Holmes <richard.gordon.holmes@gmail.com>
2024-01-10 20:10:58 +0000

    Move preset test node strings to separate file

---
+++ webapp/scripts/test_resources/node_strings.ts
@@ -0,0 +1,14 @@
+export const PLUS_EMPTY: string =
+    `VariableNode("Plus", List(SubExprNode(ExprChoiceNode()), SubExprNode(ExprChoiceNode())))`;
+export const PLUS_LEFT_NUM_RIGHT_EMPTY: string =
+    `VariableNode("Plus", List(SubExprNode(VariableNode("Num", List(LiteralNode("")))), SubExprNode(ExprChoiceNode())))`;
+export const PLUS_LEFT_FILLED_NUM_RIGHT_EMPTY: string =
+    `VariableNode("Plus", List(SubExprNode(VariableNode("Num", List(LiteralNode("56")))), SubExprNode(ExprChoiceNode())))`;
+
+
+export const TIMES_EMPTY: string =
+    `VariableNode("Times", List(SubExprNode(ExprChoiceNode()), SubExprNode(ExprChoiceNode())))`;
+export const TIMES_LEFT_NUM_RIGHT_EMPTY: string =
+    `VariableNode("Times", List(SubExprNode(VariableNode("Num", List(LiteralNode("")))), SubExprNode(ExprChoiceNode())))`;
+export const TIMES_LEFT_FILLED_NUM_RIGHT_EMPTY: string =
+    `VariableNode("Times", List(SubExprNode(VariableNode("Num", List(LiteralNode("56")))), SubExprNode(ExprChoiceNode())))`;
