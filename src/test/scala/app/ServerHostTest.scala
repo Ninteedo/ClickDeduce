a8c7a1f4f89a0fd9bfd41894809d565c84584405
Richard Holmes <richard.gordon.holmes@gmail.com>
2024-01-16 00:07:17 +0000

    Enclose entire network behaviour in try-catch in ServerHostTest

--- src/test/scala/app/ServerHostTest.scala
+++ src/test/scala/app/ServerHostTest.scala
@@ -19,35 +19,35 @@ class ServerHostTest extends AnyFunSuite with Matchers {
       serverThread.start()
 
       Thread.sleep(1000) // allows time for the server to start
+
+      failAfter(Span(1000, Millis)) {
+        noException should be thrownBy {
+          val socket = new Socket("localhost", port)
+          val request = "GET / HTTP/1.1\r\nHost: localhost\r\n\r\n"
+          socket.getOutputStream.write(request.getBytes)
+          socket.getOutputStream.flush()
+          socket.setSoTimeout(1000)
+          val in = socket.getInputStream
+          val reader = new java.io.BufferedReader(new java.io.InputStreamReader(in))
+          val response = new StringBuilder()
+          var line = reader.readLine()
+          while (line != null) {
+            if (line.contains("HTTP/1.1 200 OK")) {
+              response.append(line)
+              line = null
+            } else {
+              response.append(line)
+              line = reader.readLine()
+            }
+          }
+          response.toString should include("HTTP/1.1 200 OK")
+          socket.close()
+        }
+      }
     } catch {
       case e: ConnectException =>
         serverThread.interrupt()
         cancel("Unable to host a server on this environment. Test skipped.")
-    }
-
-    failAfter(Span(1000, Millis)) {
-      noException should be thrownBy {
-        val socket = new Socket("localhost", port)
-        val request = "GET / HTTP/1.1\r\nHost: localhost\r\n\r\n"
-        socket.getOutputStream.write(request.getBytes)
-        socket.getOutputStream.flush()
-        socket.setSoTimeout(1000)
-        val in = socket.getInputStream
-        val reader = new java.io.BufferedReader(new java.io.InputStreamReader(in))
-        val response = new StringBuilder()
-        var line = reader.readLine()
-        while (line != null) {
-          if (line.contains("HTTP/1.1 200 OK")) {
-            response.append(line)
-            line = null
-          } else {
-            response.append(line)
-            line = reader.readLine()
-          }
-        }
-        response.toString should include("HTTP/1.1 200 OK")
-        socket.close()
-      }
     }
 
     serverThread.interrupt()
