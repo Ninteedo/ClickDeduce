66be93e1495e09afad6dd5f6327e558e975a7942
Richard Holmes <richard.gordon.holmes@gmail.com>
2024-02-06 23:09:36 +0000

    Begin updating actions.mock.test.ts

--- webapp/scripts/test/actions.mock.test.ts
+++ webapp/scripts/test/actions.mock.test.ts
@@ -1,13 +1,14 @@
 import {beforeEach, describe, expect, test} from "@jest/globals";
 import {initialise} from "../initialise";
-import {contextMenuSelect, getLeftmostExprDropdown, selectExprOption, slightDelay} from "./helper";
+import {contextMenuSelect, getLeftmostExprDropdown, loadHtmlTemplate, selectExprOption, slightDelay} from "./helper";
 import {doStartNodeBlank} from "../actions";
 import * as NS from "../../test_resources/node_strings";
 
-beforeEach(async () => {
-    resetRequestTracking();
-    document.body.innerHTML = defaultHtml;
-    await initialise(true);
+const indexHtml = loadHtmlTemplate('../pages/index');
+
+beforeEach(() => {
+    document.body.innerHTML = indexHtml;
+    initialise(true);
 });
 
 describe("start new node button behaves correctly", () => {
@@ -24,17 +25,17 @@ describe("start new node button behaves correctly", () => {
         expect(getRequestsReceived()).toContainEqual({url: 'start-node-blank', request: correctRequest});
     });
 
-    // test("the contents of the tree div are replaced with the new tree HTML", async () => {
+    // test("the contents of the tree div are replaced with the new tree HTML", () => {
     //     const startNodeButton = document.getElementById('start-node-button') as HTMLButtonElement;
     //     startNodeButton.click();
     //
-    //     await slightDelay();
+    //     slightDelay();
     //
     //     const tree = document.getElementById('tree');
     //     expect(removeWhitespace(tree.innerHTML)).toEqual(removeWhitespace(startNodeBlankArithHTML));
     // });
 
-    test("the request made respects the selected language", async () => {
+    test("the request made respects the selected language", () => {
         const langSelector = document.getElementById('lang-selector') as HTMLSelectElement;
         langSelector.selectedIndex = 1;
         langSelector.dispatchEvent(new Event('change'));
@@ -48,23 +49,23 @@ describe("start new node button behaves correctly", () => {
             body: JSON.stringify({langName: langSelectorLanguages[1]})
         }
 
-        await slightDelay();
+        slightDelay();
 
         expect(getRequestsReceived()).toContainEqual({url: 'start-node-blank', request: correctRequest});
     });
 
-    test("changing the selected language causes an identity action", async () => {
+    test("changing the selected language causes an identity action", () => {
         const startNodeButton = document.getElementById('start-node-button') as HTMLButtonElement;
         startNodeButton.click();
 
-        await slightDelay();
+        slightDelay();
 
         const langSelector = document.getElementById('lang-selector') as HTMLSelectElement;
         console.log(langSelector.selectedIndex);
         langSelector.selectedIndex = 1;
         langSelector.dispatchEvent(new Event('change'));
 
-        await slightDelay();
+        slightDelay();
 
         checkActionRequestExecuted("IdentityAction", langSelectorLanguages[1], "edit",
             "ExprChoiceNode()", "", []);
@@ -72,11 +73,11 @@ describe("start new node button behaves correctly", () => {
 });
 
 describe("selecting an option from the root expr dropdown behaves correctly", () => {
-    beforeEach(async () => {
-        await doStartNodeBlank(mockEvent);
+    beforeEach(() => {
+        doStartNodeBlank(mockEvent);
     });
 
-    test("select expr dropdown is available", async () => {
+    test("select expr dropdown is available", () => {
         expect.assertions(1);
 
         const exprDropdown = getLeftmostExprDropdown();
@@ -84,8 +85,8 @@ describe("selecting an option from the root expr dropdown behaves correctly", ()
     });
 
     function selectOptionResultsInCorrectRequest(exprName: string) {
-        test("selecting the " + exprName + " option makes the correct request to the server", async () => {
-            await selectExprOption(getLeftmostExprDropdown(), exprName);
+        test("selecting the " + exprName + " option makes the correct request to the server", () => {
+            selectExprOption(getLeftmostExprDropdown(), exprName);
             checkActionRequestExecuted("SelectExprAction", langSelectorLanguages[0], "edit",
                 "ExprChoiceNode()", "", [exprName]);
         });
@@ -99,15 +100,15 @@ describe("selecting an option from the root expr dropdown behaves correctly", ()
 describe("selecting an option from a non-root expr dropdown behaves correctly", () => {
     const dummyNodeString: string = NS.PLUS_EMPTY;
 
-    beforeEach(async () => {
-        await doStartNodeBlank(mockEvent);
+    beforeEach(() => {
+        doStartNodeBlank(mockEvent);
 
         setActionFetchResponse(dummyNodeString, plusNodeArithHTML);
 
-        await selectExprOption(getLeftmostExprDropdown(), "Plus");
+        selectExprOption(getLeftmostExprDropdown(), "Plus");
     });
 
-    test("left and right dropdowns are available", async () => {
+    test("left and right dropdowns are available", () => {
         expect.assertions(7);
 
         const dropdowns = document.querySelectorAll('.expr-selector-dropdown:not([readonly])');
@@ -124,22 +125,22 @@ describe("selecting an option from a non-root expr dropdown behaves correctly", 
     });
 
     function selectOptionResultsInCorrectRequest(exprName: string) {
-        test("selecting the left " + exprName + " option makes the correct request to the server", async () => {
+        test("selecting the left " + exprName + " option makes the correct request to the server", () => {
             expect.assertions(1);
 
             const leftDropdown = document.querySelectorAll('.expr-selector-container:not([readonly])').item(0) as HTMLDivElement;
             console.log(exprName)
-            await selectExprOption(leftDropdown, exprName);
+            selectExprOption(leftDropdown, exprName);
 
             checkActionRequestExecuted("SelectExprAction", langSelectorLanguages[0], "edit",
                 dummyNodeString, "0", [exprName]);
         });
 
-        test("selecting the right " + exprName + " option makes the correct request to the server", async () => {
+        test("selecting the right " + exprName + " option makes the correct request to the server", () => {
             expect.assertions(1);
 
             const rightSelector = document.querySelectorAll('.expr-selector-container:not([readonly])').item(1) as HTMLDivElement;
-            await selectExprOption(rightSelector, exprName);
+            selectExprOption(rightSelector, exprName);
 
             checkActionRequestExecuted("SelectExprAction", langSelectorLanguages[0], "edit",
                 dummyNodeString, "1", [exprName]);
@@ -167,22 +168,22 @@ describe("entering text into a literal input behaves correctly", () => {
             )
     };
 
-    beforeEach(async () => {
-        await doStartNodeBlank(mockEvent);
+    beforeEach(() => {
+        doStartNodeBlank(mockEvent);
 
         setActionFetchResponse(dummyNodeString, numNodeArithHTML);
 
-        await selectExprOption(getLeftmostExprDropdown(), "Num");
+        selectExprOption(getLeftmostExprDropdown(), "Num");
     });
 
-    test("input is available", async () => {
+    test("input is available", () => {
         expect.assertions(1);
 
         const input = document.querySelector('input[type="text"]');
         expect(input).toBeInstanceOf(HTMLInputElement);
     });
 
-    test("entering text makes the correct request to the server", async () => {
+    test("entering text makes the correct request to the server", () => {
         expect.assertions(1);
 
         const input = document.querySelector('input.literal[type="text"]') as HTMLInputElement;
@@ -193,7 +194,7 @@ describe("entering text into a literal input behaves correctly", () => {
             dummyNodeString, "0", ["foo"]);
     });
 
-    test("entering text multiple times makes the correct requests to the server", async () => {
+    test("entering text multiple times makes the correct requests to the server", () => {
         expect.assertions(3);
 
         const bar = "bar";
@@ -207,7 +208,7 @@ describe("entering text into a literal input behaves correctly", () => {
         checkActionRequestExecuted("EditLiteralAction", langSelectorLanguages[0], "edit",
             dummyNodeString, "0", [foo]);
 
-        await slightDelay();
+        slightDelay();
 
         input = document.querySelector('input.literal[type="text"]') as HTMLInputElement;
 
@@ -220,7 +221,7 @@ describe("entering text into a literal input behaves correctly", () => {
             fooActionFetchResponse.nodeString, "0", [bar]);
     });
 
-    test("if the input text is the same as it was before (blank), no server request is made", async () => {
+    test("if the input text is the same as it was before (blank), no server request is made", () => {
         expect.assertions(2);
 
         const input = document.querySelector('input[type="text"]') as HTMLInputElement;
@@ -242,7 +243,7 @@ describe("entering text into a literal input behaves correctly", () => {
         expect(getRequestsReceived()).toHaveLength(initialRequestsReceived);
     });
 
-    test("if the input text is the same as it was before (not blank), no server request is made", async () => {
+    test("if the input text is the same as it was before (not blank), no server request is made", () => {
         expect.assertions(2);
 
         setActionFetchResponseData(fooActionFetchResponse);
@@ -251,13 +252,13 @@ describe("entering text into a literal input behaves correctly", () => {
         input.value = foo;
         input.dispatchEvent(new Event('change'));
 
-        await slightDelay();
+        slightDelay();
 
         const initialRequestsReceived = getRequestsReceived().length;
 
         input.dispatchEvent(new Event('change'));
 
-        await slightDelay();
+        slightDelay();
 
         expect(getRequestsReceived()).toHaveLength(initialRequestsReceived);
 
@@ -272,11 +273,11 @@ describe("entering text into a literal input behaves correctly", () => {
 });
 
 describe("delete, copy, and paste buttons behave correctly", () => {
-    beforeEach(async () => {
-        await prepareExampleTimesTree();
+    beforeEach(() => {
+        prepareExampleTimesTree();
     });
 
-    test("pressing delete makes the correct request to the server", async () => {
+    test("pressing delete makes the correct request to the server", () => {
         expect.assertions(1);
 
         const element = document.querySelector('[data-tree-path="0"]') as HTMLElement;
@@ -289,7 +290,7 @@ describe("delete, copy, and paste buttons behave correctly", () => {
             NS.TIMES_LEFT_FILLED_NUM_RIGHT_EMPTY, "0", []);
     });
 
-    test("pressing copy does not make a request to the server", async () => {
+    test("pressing copy does not make a request to the server", () => {
         expect.assertions(1);
 
         const element = document.querySelector('[data-tree-path="0"]') as HTMLElement;
@@ -303,7 +304,7 @@ describe("delete, copy, and paste buttons behave correctly", () => {
         expect(getRequestsReceived().length).toEqual(initialRequestsReceived);
     });
 
-    test("clicking paste has no effect before copying something", async () => {
+    test("clicking paste has no effect before copying something", () => {
         expect.assertions(1);
 
         const element = document.querySelector('[data-tree-path="0"]') as HTMLElement;
@@ -317,7 +318,7 @@ describe("delete, copy, and paste buttons behave correctly", () => {
         expect(getRequestsReceived().length).toEqual(initialRequestsReceived);
     });
 
-    test("clicking paste on same element after copying it makes the correct request to the server", async () => {
+    test("clicking paste on same element after copying it makes the correct request to the server", () => {
         const element = document.querySelector('[data-tree-path="0"]') as HTMLElement;
         contextMenuSelect(element);
 
@@ -333,7 +334,7 @@ describe("delete, copy, and paste buttons behave correctly", () => {
             NS.TIMES_LEFT_FILLED_NUM_RIGHT_EMPTY, "0", ["VariableNode(\"Num\", List(LiteralNode(\"4\")))"]);
     });
 
-    test("clicking paste on another element after copying one makes the correct request to the server", async () => {
+    test("clicking paste on another element after copying one makes the correct request to the server", () => {
         const element1 = document.querySelector('.subtree[data-tree-path="0"]') as HTMLElement;
         contextMenuSelect(element1);
 
@@ -350,7 +351,7 @@ describe("delete, copy, and paste buttons behave correctly", () => {
             NS.TIMES_LEFT_FILLED_NUM_RIGHT_EMPTY, "1", ["VariableNode(\"Num\", List(LiteralNode(\"4\")))"]);
     });
 
-    test("clicking paste after changing tree state makes the correct request to the server", async () => {
+    test("clicking paste after changing tree state makes the correct request to the server", () => {
         contextMenuSelect(document.querySelector('[data-tree-path="0"]'));
         document.getElementById('copy-button').click();
 
@@ -365,32 +366,32 @@ describe("delete, copy, and paste buttons behave correctly", () => {
 });
 
 describe("mode radio buttons behave correctly", () => {
-    beforeEach(async () => {
-        await doStartNodeBlank(mockEvent);
+    beforeEach(() => {
+        doStartNodeBlank(mockEvent);
     });
 
-    test("the initial mode is edit", async () => {
+    test("the initial mode is edit", () => {
         expect(document.getElementById('edit-mode-radio').getAttributeNames()).toContain('checked');
     });
 
-    test("clicking the type-check mode button makes the correct request to the server", async () => {
+    test("clicking the type-check mode button makes the correct request to the server", () => {
         document.getElementById('type-check-mode-radio').click();
 
         checkActionRequestExecuted("IdentityAction", langSelectorLanguages[0], "type-check",
             "ExprChoiceNode()", "", []);
     });
 
-    test("clicking the eval mode button makes the correct request to the server", async () => {
+    test("clicking the eval mode button makes the correct request to the server", () => {
         document.getElementById('eval-mode-radio').click();
 
         checkActionRequestExecuted("IdentityAction", langSelectorLanguages[0], "eval",
             "ExprChoiceNode()", "", []);
     });
 
-    test("clicking the edit mode button makes the correct request to the server", async () => {
+    test("clicking the edit mode button makes the correct request to the server", () => {
         setActionFetchResponse("ExprChoiceNode()", plusNodeArithHTML);
         document.getElementById('type-check-mode-radio').click();
-        await slightDelay();
+        slightDelay();
 
         document.getElementById('edit-mode-radio').click();
 
@@ -398,10 +399,10 @@ describe("mode radio buttons behave correctly", () => {
             "ExprChoiceNode()", "", []);
     });
 
-    test("after selecting a mode, future requests are made with that mode", async () => {
+    test("after selecting a mode, future requests are made with that mode", () => {
         document.getElementById('type-check-mode-radio').click();
 
-        await selectExprOption(getLeftmostExprDropdown(), "Num");
+        selectExprOption(getLeftmostExprDropdown(), "Num");
 
         checkActionRequestExecuted("SelectExprAction", langSelectorLanguages[0], "type-check",
             "ExprChoiceNode()", "", ["Num"]);
