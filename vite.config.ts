bc33367d7f2178fcc714f59716b39fce8bf9159c
Richard Holmes <richard.gordon.holmes@gmail.com>
2024-09-24 17:36:22 +0100

    Move ./build to ./webapp/build and don't include random strings in output file names

--- vite.config.ts
+++ vite.config.ts
@@ -8,11 +8,21 @@ export default defineConfig({
   plugins: [scalaJSPlugin(), react()],
   root: 'webapp',
   build: {
-    outDir: '../build',
+    outDir: 'build',
     rollupOptions: {
       input: {
         index: 'webapp/index.html',
         guide: 'webapp/guide.html',
+      },
+      output: {
+        entryFileNames: 'bundle.js',
+        chunkFileNames: '[name].js',
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name && assetInfo.name.endsWith('.css')) {
+            return 'bundle.css'
+          }
+          return 'bundle-[name].[ext]'
+        }
       }
     }
   },
