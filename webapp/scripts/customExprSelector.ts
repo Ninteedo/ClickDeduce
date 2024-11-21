9859be79412dfe8a2fbe80c8a0db2cd5e46442f1
Richard Holmes <richard.gordon.holmes@gmail.com>
2024-11-21 12:14:18 +0000

    Update example expression selector to use class

--- webapp/scripts/customExprSelector.ts
+++ webapp/scripts/customExprSelector.ts
@@ -12,16 +12,18 @@ const DOWN_ARROW = '&#9660;';
 
 
 class CustomExprSelector {
-    private readonly container: HTMLDivElement;
-    private readonly input: HTMLInputElement;
+    protected readonly container: HTMLDivElement;
+    protected readonly input: HTMLInputElement;
     private readonly button: HTMLButtonElement;
     private readonly dropdown: HTMLDivElement;
+    private readonly options: HTMLLIElement[];
 
     constructor(container: HTMLDivElement) {
         this.container = container;
         this.input = container.querySelector('.expr-selector-input') as HTMLInputElement;
         this.button = container.querySelector('.expr-selector-button') as HTMLButtonElement;
         this.dropdown = container.querySelector('.expr-selector-dropdown') as HTMLDivElement;
+        this.options = Array.from(this.dropdown.querySelectorAll('ul > li')) as HTMLLIElement[];
 
         this.setup();
     }
@@ -40,13 +42,13 @@ class CustomExprSelector {
 
         this.dropdown.addEventListener('mousedown', () => this.setInteractingWithDropdown(true));
         this.dropdown.addEventListener('mouseup', () => this.setInteractingWithDropdown(false));
+        this.dropdown.addEventListener('wheel', evt => this.handleDropdownWheel(evt));
 
         this.setupOptionListeners();
     }
 
     private setupOptionListeners(): void {
-        const options = this.dropdown.querySelectorAll('ul > li');
-        options.forEach(option => {
+        this.options.forEach(option => {
             if (!(option instanceof HTMLLIElement)) {
                 throw new Error('Option was not an HTMLLIElement');
             }
@@ -57,10 +59,9 @@ class CustomExprSelector {
         });
     }
 
-    private updateDropdown(): void {
+    protected updateDropdown(): void {
         const filterText = this.input.value.toLowerCase();
-        const options = Array.from(this.dropdown.querySelectorAll('ul > li')) as HTMLLIElement[];
-        options.forEach((option) => {
+        this.options.forEach((option) => {
             const matches = option.innerText.toLowerCase().includes(filterText);
             matches ? this.showOption(option) : this.hideOption(option);
         });
@@ -79,13 +80,15 @@ class CustomExprSelector {
 
     private handleBlur() {
         setTimeout(() => {
-            if (!this.isDropdownInteracting) {
+            if (this.isDropdownInteracting) {
+                setTimeout(() => this.input.focus(), 0);
+            } else {
                 this.hideDropdown();
             }
         });
     }
 
-    private toggleDropdown() {
+    protected toggleDropdown() {
         if (this.isDropdownVisible()) {
             this.hideDropdown();
         } else {
@@ -93,14 +96,18 @@ class CustomExprSelector {
         }
     }
 
-    private showDropdown() {
+    protected showDropdown() {
         this.dropdown.classList.add(DROPDOWN_VISIBLE_CLASS);
+        this.dropdown.scrollTop = 0;
         this.button.innerHTML = UP_ARROW;
+        this.container.classList.add(SELECTOR_FOCUS_CLASS);
+        this.highlightOption(0);
     }
 
-    private hideDropdown() {
+    protected hideDropdown() {
         this.dropdown.classList.remove(DROPDOWN_VISIBLE_CLASS);
         this.button.innerHTML = DOWN_ARROW;
+        this.container.classList.remove(SELECTOR_FOCUS_CLASS);
     }
 
     private isDropdownVisible() {
@@ -116,10 +123,16 @@ class CustomExprSelector {
     }
 
     private highlightOption(index: number) {
-        const options = Array.from(this.getVisibleOptions());
+        if (!this.isDropdownVisible()) {
+            this.showDropdown();
+        }
+
+        const options = this.getVisibleOptions();
         options.forEach((opt) => opt.classList.remove(OPTION_HIGHLIGHT_CLASS));
-        if (options[index]) {
-            options[index].classList.add(OPTION_HIGHLIGHT_CLASS);
+        const option = options[index];
+        if (option) {
+            option.classList.add(OPTION_HIGHLIGHT_CLASS);
+            option.scrollIntoView({block: 'nearest'});
         }
     }
 
@@ -138,15 +151,14 @@ class CustomExprSelector {
         }
     }
 
-    private selectOption(option: HTMLLIElement) {
+    protected selectOption(option: HTMLLIElement) {
         this.input.value = option.innerText;
-        this.hideDropdown();
         handleExprSelectorChoice(this.container, option.getAttribute('data-value'));
         this.hideDropdown();
     }
 
     private getVisibleOptions(): HTMLLIElement[] {
-        return Array.from(this.dropdown.querySelectorAll('ul > li:not(.hidden)')) as HTMLLIElement[];
+        return this.options.filter((opt) => !opt.classList.contains(OPTION_HIDDEN_CLASS));
     }
 
     private isDropdownInteracting = false;
@@ -165,6 +177,13 @@ class CustomExprSelector {
         return this.getVisibleOptions().find((opt) =>
             opt.classList.contains(OPTION_HIGHLIGHT_CLASS)
         );
+    }
+
+    private handleDropdownWheel(evt: WheelEvent) {
+        if (!((this.dropdown.scrollTop === 0 && evt.deltaY < 0)
+            || (this.dropdown.scrollTop === this.dropdown.scrollHeight - this.dropdown.clientHeight && evt.deltaY > 0))) {
+            evt.stopPropagation();
+        }
     }
 }
 
@@ -241,175 +260,32 @@ function setupTermSelector(termSelectorContainer: HTMLDivElement): CustomExprSel
     return new CustomExprSelector(termSelectorContainer);
 }
 
+class ExampleExprSelector extends CustomExprSelector {
+    private readonly output: HTMLDivElement;
+
+    constructor(container: HTMLDivElement, output: HTMLDivElement) {
+        super(container);
+        this.output = output;
+    }
+
+    protected override selectOption(option: HTMLLIElement) {
+        this.input.value = option.innerText;
+        this.output.textContent = option.textContent;
+        this.hideDropdown();
+        this.input.focus();
+        this.container.classList.add(SELECTOR_FOCUS_CLASS);
+    }
+
+    private clearOutput() {
+        this.output.textContent = '?';
+    }
+
+    protected override updateDropdown() {
+        super.updateDropdown();
+        this.clearOutput();
+    }
+}
+
 export function setupExampleSelector(termSelectorContainer: HTMLDivElement): void {
-    const input = getSelectorInput(termSelectorContainer);
-    const button = getSelectorButton(termSelectorContainer);
-    const dropdown = getSelectorDropdown(termSelectorContainer);
-    const output = document.getElementById("expr-selector-output");
-
-    if (!output) throw new Error("Could not find output element");
-
-    function selectOption(option: HTMLLIElement): void {
-        input.value = option.innerText;
-        output!.textContent = option.textContent;
-        hideExprSelectorDropdown(termSelectorContainer);
-    }
-
-    function clearOutput(): void {
-        output!.textContent = "?";
-    }
-
-    input.addEventListener('input', () => {
-        updateExprSelectorDropdown(termSelectorContainer)
-        clearOutput();
-    });
-    input.addEventListener('keydown', (event) => {
-        if (event.key === 'Enter') {
-            const selectedIndex = getExprSelectorOptionHighlight(termSelectorContainer, false);
-            const selectedOption = getSelectorOptions(termSelectorContainer)[selectedIndex];
-            if (selectedOption) {
-                selectOption(selectedOption);
-            }
-        } else if (event.key === 'ArrowDown') {
-            moveSelectorOptionHighlight(termSelectorContainer, 1);
-        } else if (event.key === 'ArrowUp') {
-            moveSelectorOptionHighlight(termSelectorContainer, -1);
-        }
-    });
-    input.addEventListener('focus', () => showExprSelectorDropdown(termSelectorContainer));
-    input.addEventListener('blur', () => hideExprSelectorDropdown(termSelectorContainer));
-
-    button.addEventListener('click', () => input.focus());
-
-    const selectorOptions = Array.from(dropdown.querySelectorAll('ul > li'));
-    selectorOptions.forEach(option => {
-        if (!(option instanceof HTMLLIElement)) {
-            throw new Error('Selector option was not an HTMLLIElement');
-        }
-
-        option.addEventListener('mousedown', event => {
-            event.preventDefault();
-            selectOption(option);
-        });
-        option.classList.add('expr-selector-option');
-    });
-}
-
-function updateExprSelectorDropdown(selectorDiv: HTMLDivElement, keepOpenWhenEmpty: boolean = true) {
-    const input = getSelectorInput(selectorDiv);
-
-    if (input.value === '' && !keepOpenWhenEmpty) {
-        if (isExprSelectorDropdownVisible(selectorDiv)) {
-            toggleExprSelectorDropdownDisplay(selectorDiv);
-        }
-        return;
-    }
-
-    showExprSelectorDropdown(selectorDiv);
-
-    const filterText = input.value.toLowerCase();
-    getSelectorOptions(selectorDiv).forEach(option => {
-        const aliases = option.getAttribute('data-aliases')?.toLowerCase().split(',');
-        if (option.innerHTML.toLowerCase().includes(filterText) || (aliases && aliases.some(alias => alias.includes(filterText)))) {
-            showSelectorOption(option);
-        } else {
-            hideSelectorOption(option);
-        }
-    });
-
-    setExprSelectorOptionHighlight(selectorDiv, 0);
-}
-
-function setExprSelectorOptionHighlight(selectorDiv: HTMLDivElement, highlightIndex: number) {
-    const options = getSelectorOptions(selectorDiv);
-    options.forEach(option => option.classList.remove(OPTION_HIGHLIGHT_CLASS));
-    const filtered = visibleSelectorOptions(selectorDiv);
-    if (highlightIndex >= 0 && highlightIndex < filtered.length) {
-        filtered[highlightIndex].classList.add(OPTION_HIGHLIGHT_CLASS);
-    }
-}
-
-function getExprSelectorOptionHighlight(selectorDiv: HTMLDivElement, ignoreHidden: boolean): number {
-    let options: HTMLLIElement[];
-    if (ignoreHidden) {
-        options = visibleSelectorOptions(selectorDiv);
-    } else {
-        options = getSelectorOptions(selectorDiv);
-    }
-    return options.findIndex(option => option.classList.contains(OPTION_HIGHLIGHT_CLASS));
-}
-
-function moveSelectorOptionHighlight(selectorDiv: HTMLDivElement, offset: number): void {
-    const filtered = visibleSelectorOptions(selectorDiv);
-    const currentHighlightIndex = getExprSelectorOptionHighlight(selectorDiv, true);
-    let newHighlightIndex = (currentHighlightIndex + offset) % filtered.length;
-    if (newHighlightIndex < 0) newHighlightIndex += filtered.length;  // wrap around from -1 to the end
-    setExprSelectorOptionHighlight(selectorDiv, newHighlightIndex);
-}
-
-function toggleExprSelectorDropdownDisplay(selectorDiv: HTMLDivElement) {
-    if (isExprSelectorDropdownVisible(selectorDiv)) {
-        hideExprSelectorDropdown(selectorDiv);
-    } else {
-        showExprSelectorDropdown(selectorDiv);
-    }
-}
-
-function showExprSelectorDropdown(selectorDiv: HTMLDivElement) {
-    if (isExprSelectorDropdownVisible(selectorDiv)) return;
-    getSelectorDropdown(selectorDiv).classList.add(DROPDOWN_VISIBLE_CLASS);
-    getSelectorDropdown(selectorDiv).scrollTop = 0;
-    getSelectorButton(selectorDiv).innerHTML = UP_ARROW;
-    getSelectorButton(selectorDiv).disabled = true;
-    updateExprSelectorDropdown(selectorDiv, true);
-    selectorDiv.classList.add(SELECTOR_FOCUS_CLASS);
-}
-
-function hideExprSelectorDropdown(selectorDiv: HTMLDivElement): void {
-    if (!isExprSelectorDropdownVisible(selectorDiv)) return;
-    getSelectorDropdown(selectorDiv).classList.remove(DROPDOWN_VISIBLE_CLASS);
-    getSelectorButton(selectorDiv).innerHTML = DOWN_ARROW;
-    getSelectorButton(selectorDiv).disabled = false;
-    getSelectorOptions(selectorDiv).forEach(option => {
-        option.classList.remove(OPTION_HIGHLIGHT_CLASS);
-        hideSelectorOption(option);
-    });
-    selectorDiv.classList.remove(SELECTOR_FOCUS_CLASS);
-}
-
-function isExprSelectorDropdownVisible(selectorDiv: HTMLDivElement): boolean {
-    return getSelectorDropdown(selectorDiv).classList.contains(DROPDOWN_VISIBLE_CLASS);
-}
-
-function showSelectorOption(option: HTMLLIElement): void {
-    option.classList.remove(OPTION_HIDDEN_CLASS);
-}
-
-function hideSelectorOption(option: HTMLLIElement): void {
-    option.classList.add(OPTION_HIDDEN_CLASS);
-}
-
-function isSelectorOptionHidden(option: HTMLLIElement): boolean {
-    return option.classList.contains(OPTION_HIDDEN_CLASS);
-}
-
-function visibleSelectorOptions(selectorDiv: HTMLDivElement): HTMLLIElement[] {
-    return getSelectorOptions(selectorDiv).filter(option => !isSelectorOptionHidden(option));
-}
-
-function getSelectorInput(selectorDiv: HTMLDivElement): HTMLInputElement {
-    return selectorDiv.querySelector('.expr-selector-input') as HTMLInputElement;
-}
-
-function getSelectorButton(selectorDiv: HTMLDivElement): HTMLButtonElement {
-    return selectorDiv.querySelector('.expr-selector-button') as HTMLButtonElement;
-}
-
-function getSelectorDropdown(selectorDiv: HTMLDivElement): HTMLDivElement {
-    return selectorDiv.querySelector('.expr-selector-dropdown') as HTMLDivElement;
-}
-
-function getSelectorOptions(selectorDiv: HTMLDivElement): HTMLLIElement[] {
-    const dropdown = selectorDiv.querySelector('.expr-selector-dropdown') as HTMLDivElement;
-    return Array.from(dropdown.querySelectorAll('ul > li'));
+    new ExampleExprSelector(termSelectorContainer, document.getElementById("expr-selector-output") as HTMLDivElement);
 }
