eccc8de014a6687c012c6281f86e1e2dc323458a
Richard Holmes <richard.gordon.holmes@gmail.com>
2025-01-12 19:05:42 +0000

    Add animation to hovering over completed tasks

--- webapp/scripts/tasks.ts
+++ webapp/scripts/tasks.ts
@@ -2,7 +2,10 @@ import {checkTaskFulfilled, getTasksList} from "./serverRequest";
 import {markHasCompletedFirstLangTasks} from "./attention";
 import {getLangSelector} from "./globals/elements";
 
-let fulfilledTasks: string[] = [];
+const FULFILLED_CLASS = "fulfilled";
+const TASK_CLASS = "task";
+const DESCRIPTION_CLASS = "description";
+const HIDDEN_CLASS = "hidden";
 
 export interface Task {
     name: string;
@@ -10,64 +13,126 @@ export interface Task {
     difficulty: number;
 }
 
-let taskList: Task[] = [];
-
 export function updateTaskList(lang: string, nodeString: string): void {
-    const tasks: Task[] = getTasksList(lang);
-    let allFulfilled = true;
-    taskList = [];
-    for (const task of tasks) {
-        taskList.push(task);
-        const {name} = task;
+    getTaskManager().updateTaskList(lang, nodeString);
+}
 
-        if (fulfilledTasks.includes(name)) {
-            continue;
+let taskManager: TaskManager | undefined = undefined;
+
+function getTaskManager(): TaskManager {
+    if (taskManager === undefined) {
+        taskManager = new TaskManager();
+    }
+    return taskManager;
+}
+
+function isFulfilled(taskDiv: HTMLDivElement): boolean {
+    return taskDiv.classList.contains(FULFILLED_CLASS);
+}
+
+class TaskManager {
+    private currentTasks: Task[] = [];
+    private fulfilledTasks: string[] = [];
+    private lastLang: string | undefined = undefined;
+
+    private readonly tasksDiv: HTMLDivElement;
+
+    constructor() {
+        this.tasksDiv = document.getElementById("tasks") as HTMLDivElement;
+        if (!this.tasksDiv) {
+            throw new Error("No tasks div found");
+        }
+    }
+
+    public updateTaskList(lang: string, nodeString: string): void {
+        const tasks: Task[] = getTasksList(lang);
+        let allFulfilled = true;
+        this.currentTasks = [];
+        for (const task of tasks) {
+            this.currentTasks.push(task);
+            const {name} = task;
+
+            if (this.fulfilledTasks.includes(name)) continue;
+
+            const fulfilled = checkTaskFulfilled(lang, name, nodeString);
+            if (fulfilled) {
+                this.fulfilledTasks.push(name);
+            } else {
+                allFulfilled = false;
+            }
         }
 
-        const fulfilled = checkTaskFulfilled(lang, name, nodeString);
-        if (fulfilled) {
-            fulfilledTasks.push(name);
+        if (this.lastLang !== lang) {
+            this.newTasksDiv();
         } else {
-            allFulfilled = false;
+            this.updateTasksDiv();
+        }
+        this.lastLang = lang;
+
+        if (getLangSelector().selectedIndex === 0 && allFulfilled) {
+            markHasCompletedFirstLangTasks();
         }
     }
-    updateTasksDiv();
 
-    if (getLangSelector().selectedIndex === 0 && allFulfilled) {
-        markHasCompletedFirstLangTasks();
+    createTaskElement({name, description, difficulty}: Task): HTMLDivElement {
+        const taskDiv = document.createElement("div");
+        taskDiv.classList.add(TASK_CLASS);
+        taskDiv.setAttribute("data-task-name", name);
+
+        const stars = "★".repeat(difficulty);
+
+        const taskName = document.createElement("h3");
+        taskName.textContent = `${stars} ${name}`;
+        taskDiv.appendChild(taskName);
+
+        const taskDescription = document.createElement("p");
+        taskDescription.classList.add(DESCRIPTION_CLASS);
+        taskDescription.textContent = description;
+        taskDiv.appendChild(taskDescription);
+
+        if (this.fulfilledTasks.includes(name)) {
+            taskDiv.classList.add(FULFILLED_CLASS);
+            taskDescription.style.maxHeight = "0";
+        }
+
+        taskDiv.addEventListener("mouseenter", () => {
+            if (taskDiv.classList.contains(FULFILLED_CLASS)) {
+                taskDescription.style.maxHeight = taskDescription.scrollHeight + "px";
+            }
+        });
+        taskDiv.addEventListener("mouseleave", () => {
+            if (taskDiv.classList.contains(FULFILLED_CLASS)) {
+                taskDescription.style.maxHeight = "0";
+            }
+        });
+
+        return taskDiv;
     }
-}
 
-function createTaskElement({name, description, difficulty}: Task): HTMLDivElement {
-    const taskDiv = document.createElement("div");
-    taskDiv.classList.add("task");
+    newTasksDiv(): void {
+        this.tasksDiv.innerHTML = "";
 
-    const stars = "★".repeat(difficulty);
-
-    taskDiv.innerHTML = `
-      <h3>${stars} ${name}</h3>
-      <p class="description">${description}</p>
-    `;
-    if (fulfilledTasks.includes(name)) {
-        taskDiv.classList.add("fulfilled");
-    }
-    return taskDiv;
-}
-
-function updateTasksDiv(): void {
-    const tasksDiv = getTasksDiv();
-    tasksDiv.innerHTML = "";
-
-    if (taskList.length === 0) {
-        tasksDiv.classList.add("hidden");
-    } else {
-        tasksDiv.classList.remove("hidden");
-        for (const task of taskList) {
-            tasksDiv.appendChild(createTaskElement(task));
+        if (this.currentTasks.length === 0) {
+            this.tasksDiv.classList.add(HIDDEN_CLASS);
+        } else {
+            this.tasksDiv.classList.remove(HIDDEN_CLASS);
+            for (const task of this.currentTasks) {
+                this.tasksDiv.appendChild(this.createTaskElement(task));
+            }
         }
     }
-}
 
-function getTasksDiv(): HTMLDivElement {
-    return document.getElementById("tasks") as HTMLDivElement;
+    updateTasksDiv(): void {
+        for (const task of this.currentTasks) {
+            const taskDiv = this.tasksDiv.querySelector(`[data-task-name="${task.name}"]`);
+            if (taskDiv && this.fulfilledTasks.includes(task.name) && taskDiv instanceof HTMLDivElement && !isFulfilled(taskDiv)) {
+                taskDiv.classList.add(FULFILLED_CLASS);
+                const taskDescription = taskDiv.querySelector(".description");
+                if (taskDescription instanceof HTMLElement) {
+                    taskDescription.style.maxHeight = taskDescription.scrollHeight + "px";
+                    setTimeout(() => taskDescription.style.maxHeight = "0", 0);
+                }
+            }
+        }
+    }
 }
