a9ce9fb48a7be70c3f4e0ef034d836ffcd456a1c
Richard Holmes <richard.gordon.holmes@gmail.com>
2025-01-30 12:15:44 +0000

    Remove unused ScalaJsEntry.ts

--- webapp/scripts/ScalaJsEntry.ts
+++
@@ -1,6 +0,0 @@
-export declare const ScalaJsEntry: {
-    startNodeBlank: (langName: string) => [string, string],
-    getLangSelector: () => string,
-    processAction: (langName: string, modeName: string, actionName: string, nodeString: string, treePath: string, extraArgs: string[]) => [string, string],
-    convertToLaTeX: (langName: string, modeName: string, nodeString: string) => string,
-}
