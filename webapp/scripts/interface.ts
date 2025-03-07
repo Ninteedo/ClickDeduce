c69455b4a4562604a95e3d13fab18160d56c4196
Richard Holmes <richard.gordon.holmes@gmail.com>
2025-03-07 10:22:37 +0000

    Remove unused functions in interface.ts

--- webapp/scripts/interface.ts
+++ webapp/scripts/interface.ts
@@ -1,6 +1,5 @@
 import {copyTreeNode, deleteTreeNode, pasteTreeNode} from "./actions";
 import {redo, undo} from "./treeManipulation";
-import {compareTreePaths, parseTreePath} from "./utils";
 import {getControlsDiv, getToggleControlsButton, getTreePathOfElementOptional} from "./globals/elements";
 import {
     clearContextMenuSelectedElement,
@@ -9,7 +8,6 @@ import {
     openContextMenu
 } from "./components/contextMenu/contextMenu";
 import {getFocusedSubtreePath, setNextFocusTreePath} from "./focus";
-import {getActiveInputs} from "./activeInputs";
 import {ClassDict} from "./globals/classDict";
 import {IdDict} from "./globals/idDict";
 
@@ -56,53 +54,6 @@ function globalHandleKeyDown(e: KeyboardEvent): void {
 }
 
 document.addEventListener('keydown', globalHandleKeyDown);
-
-/**
- * Clears the highlight from the currently highlighted element.
- *
- * Also clears the contextMenuSelectedElement.
- */
-export function clearHighlight(): void {
-    document.querySelector(`.${ClassDict.HIGHLIGHT}`)?.classList.remove(ClassDict.HIGHLIGHT);
-    clearContextMenuSelectedElement();
-}
-
-/**
- * Sets the focus to the input element with the given tree path.
- * If that element is not found, will instead try to focus on the first input element in a subtree of that path.
- * If there is no input element in the subtree, will do nothing.
- */
-export function setFocusElement(path: string): void {
-    let focusedElement = getActiveInputs().find(input => compareTreePaths(path, input.getTreePath()) <= 0);
-
-    if (!focusedElement) {
-        const parsedPath = parseTreePath(path);
-
-        function isBefore(a: number[], b: number[]): boolean {
-            let i = 0;
-            while (i < a.length && i < b.length) {
-                if (a[i] < b[i]) {
-                    return true;
-                } else if (a[i] > b[i]) {
-                    return false;
-                }
-                i++;
-            }
-            return true;
-        }
-
-        function findLast<T>(arr: T[], pred: (elem: T) => boolean): T | undefined {
-            for (let i = arr.length - 1; i >= 0; i--) {
-                if (pred(arr[i])) return arr[i];
-            }
-            return undefined;
-        }
-
-        focusedElement = findLast(getActiveInputs(), input => isBefore(parseTreePath(input.getTreePath()), parsedPath));
-    }
-
-    focusedElement?.focus();
-}
 
 function setupValueTypeColourHighlightingCheckbox(): void {
     const valueTypeColourCheckbox = document.getElementById(IdDict.VALUE_HIGHLIGHTING_TOGGLE) as HTMLInputElement;
